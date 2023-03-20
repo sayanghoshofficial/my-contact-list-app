6bdb69b4ccff6b0880831ede46bda65755813971
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-20 20:07:51 +0530

    Creating form contact

--- src/components/ContactListForm.js
+++ src/components/ContactListForm.js
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import Styles from "../Styles/Home.module.css";
 
 const ContactListForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const getContactValues = () =>{
+    setName(name);
+    console.log(name);
+    setEmail(email);
+    console.log(email);
+    setNumber(number);
+    console.log(number)
+  }
+
   return (
     <div className={Styles.contactListForm}>
       <form>
-        <input type="text" placeholder="Full Name" required/>
-        <input type="number" placeholder="Phone No." required/>
-        <input type="email" placeholder="Email id" required/>
-        <button type="submit" className={Styles.contactListFormbtn}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => e.target.value}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Phone No."
+          value={email}
+          onChange={(e) => e.target.value}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email id"
+          value={number}
+          onChange={(e) => e.target.value}
+          required
+        />
+        <button
+          type="submit"
+          className={Styles.contactListFormbtn}
+          onClick={getContactValues}
+        >
           <img
             src="https://cdn-icons-png.flaticon.com/512/1057/1057240.png"
             alt="Add Contact"
