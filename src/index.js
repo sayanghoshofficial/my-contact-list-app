d5a429b6c4500f6cf93bc7c9d95e838ec9fc6866
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-23 21:22:29 +0530

    add some comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "./Styles/index.css";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root rendering 
 root.render(
   <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
     <React.StrictMode>
